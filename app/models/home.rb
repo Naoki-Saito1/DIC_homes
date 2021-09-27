f2cd85c65ea6b03d4429c883710c6568375be768
saitonaoki <reisa.naoki@icloud.com>
2021-09-27 23:35:38 +0900

    routes設定

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,2 +1,5 @@
 class Home < ApplicationRecord
+  has_many :nstations
+  has_many :stationhomes, dependent: :destroy
+  accepts_nested_attributes_for :nstations, allow_destroy: true, reject_if: :all_blank
 end
--- app/models/nstation.rb
+++ app/models/nstation.rb
@@ -1,2 +1,5 @@
 class Nstation < ApplicationRecord
+  has_many :stationhomes, dependent: :destroy
+  has_many :nstation_homes, through: :nstations, source: :home
+  belongs_to :home
 end
