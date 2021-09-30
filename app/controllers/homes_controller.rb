247ec7c1693b2d744d09864938cfc1affa815b3d
saitonaoki <reisa.naoki@icloud.com>
2021-09-30 16:03:01 +0900

    css編集

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -27,7 +27,7 @@ class HomesController < ApplicationController
     @home = Home.new(home_params)
     respond_to do |format|
       if @home.save
-        format.html { redirect_to @home, notice: "Home was successfully created." }
+        format.html { redirect_to @home, notice: "登録完了" }
         format.json { render :show, status: :created, location: @home }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +40,7 @@ class HomesController < ApplicationController
   def update
     respond_to do |format|
       if @home.update(home_params)
-        format.html { redirect_to @home, notice: "Home was successfully updated." }
+        format.html { redirect_to @home, notice: "更新完了" }
         format.json { render :show, status: :ok, location: @home }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class HomesController < ApplicationController
   def destroy
     @home.destroy
     respond_to do |format|
-      format.html { redirect_to homes_url, notice: "Home was successfully destroyed." }
+      format.html { redirect_to homes_url, notice: "削除しました" }
       format.json { head :no_content }
     end
   end
